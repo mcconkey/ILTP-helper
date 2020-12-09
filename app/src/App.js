94b0d95ffb598e20432ef845b5da6350ea8491fa
Christopher McConkey <topher@ChristophersMBP.satx.rr.com>
2020-12-09 16:03:54 -0600

    added fade

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import { Fade } from 'react-reveal';
 //import { Styles } from 'react';
 
 function App() {
   return (
     <div className="parent App">
-        <Card style={{width: '20rem'}}>
-               <Card.Title>Card Title</Card.Title>
-               <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-               </Card.Text>
-        </Card>
+      <Fade left>
+          <Card style={{width: '40rem'}}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                </Card.Text>
+                <Button variant="light">Get Started</Button>
+            </Card.Body>
+          </Card>
+        </Fade>
     </div>
   );
 }
